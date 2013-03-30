4e2d2b5ea5471cbcb51016613782d35f118acefb
Marta <conkordia-lviv@mai.lru>
2013-03-30 20:58:44 +0200

    номер 5

---
+++ Marta-Drozdovska/номер 5 (1).py
@@ -0,0 +1,9 @@
+Drozdovska Marta ALs-12,5.10 № 5
+import nltk
+pos=dict(favourable='ADJ',books'N',walk'V',accurately='ADV')
+pos['favourable']
+'ADJ'
+pos['xyz']
+
+//
+stvoryemo slovnyk dodaem v slovnuk slova probyem znayty klych xyz ale vubuvae scho takyu kluch vidsytnii
